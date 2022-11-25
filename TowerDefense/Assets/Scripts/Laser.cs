f0e8a1cb4002280f6fd18c0ab22b8f5b83e5141b
TimWaeckerle <97874901+TimWaeckerle@users.noreply.github.com>
2022-11-25 21:01:48 +0100

    Task 73
    Almost done.
    Rotation of Lasers is still missing

--- TowerDefense/Assets/Scripts/Laser.cs
+++ TowerDefense/Assets/Scripts/Laser.cs
@@ -7,28 +7,35 @@ public class Laser : MonoBehaviour
     public int laserSpeed;
     public int laserDamage;
     private Vector2 position;
+    private bool isNotMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject,2);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         transform.position = Vector2.MoveTowards(transform.position, position, laserSpeed * Time.deltaTime);
-        
+        StartCoroutine(CheckMoving());
+        if(isNotMoving)
+        {
+           Destroy(gameObject);
+        }
     }
 
-    public void setup(Vector2 position){
+    public void setup(Vector2 position)
+    {
         this.position = position;
     }
 
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
 
-        if(other.gameObject.tag == "Enemy") {
+        if (other.gameObject.tag == "Enemy")
+        {
             Enemy enemy = other.gameObject.GetComponent<Enemy>();
             if (enemy != null)
             {
@@ -43,4 +50,17 @@ public class Laser : MonoBehaviour
 
     }
 
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+     IEnumerator CheckMoving()
+     {
+         Vector3 startPos = transform.position;
+         yield return new WaitForSeconds(0.3f);
+         Vector3 finalPos = transform.position;
+         if(startPos == finalPos) isNotMoving = false;
+         else isNotMoving = true;
+     }
 }
