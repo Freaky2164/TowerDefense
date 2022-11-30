379e4316927e446ec051ad504b01e362f761e6bb
WeberWr <weber.luk@web.de>
2022-11-30 18:21:13 +0100

    adjust stuff

--- TowerDefense/Assets/Scripts/PlaceTower.cs
+++ TowerDefense/Assets/Scripts/PlaceTower.cs
@@ -10,21 +10,23 @@ class PlaceTower : MonoBehaviour
     private Camera _camera;
     private Collider2D _collider;
     private GameHandler _gameHandler;
-    private Vector2 _mousePos;
+    
     private Vector2 _relativePos;
     private Vector3 _oldPosition;
-    private bool _canMove;
+    
     private bool _dragging;
     private bool _hasToClone;
 
+    private Sprite _dragPic;
     private void Start()
     {
         _camera = Camera.main;
         _gameHandler = GameObject.Find("GameHandler").GetComponent<GameHandler>();
         _collider = GetComponent<Collider2D>();
-        _canMove = false;
+
         _dragging = false;
         _hasToClone = false;
+        
         var test = _collider.transform.position;
         _relativePos = new Vector2(test.x, test.y);
 
@@ -39,8 +41,8 @@ class PlaceTower : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0) || Input.touchCount == 1)
         {
-            _canMove = (_relativePos - mousePos).magnitude < 0.5F;
-            if (_canMove)
+            var canMove = (_relativePos - mousePos).magnitude < 0.5F;
+            if (canMove)
             {
                 _dragging = true;
                 _hasToClone = true;
@@ -50,7 +52,6 @@ class PlaceTower : MonoBehaviour
         if (_dragging)
         {
             transform.position = mousePos;
-            _mousePos = mousePos;
         }
 
         if (!Input.GetMouseButtonUp(0) && Input.touchCount > 0) return;
@@ -60,15 +61,12 @@ class PlaceTower : MonoBehaviour
             {
                 if (_gameHandler.FinancialSystem.TryBuy(300))
                 {
-                    Instantiate(tower, _mousePos, transform.rotation);
+                    Instantiate(tower, mousePos, transform.rotation); 
+                    _hasToClone = false;
                 }
             }
-
-            _hasToClone = false;
         }
-
         transform.position = _oldPosition;
-        _canMove = false;
         _dragging = false;
     }
 }
