1b07df0a2f7e8ac5b194ff48fe740ccdd7996a4a
WiCAM Johannes Methfessel <johannes.methfessel@wicam.com>
2023-04-11 15:19:52 +0200

    TD-95 Fix enemy and add simple path

---
+++ TowerDefense/Assets/Scripts/WaypointSystem/FollowSpline.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace WaypointSystem
+{
+    public class FollowSpline : MonoBehaviour
+    {
+        [SerializeField]
+        private SplineContainer path;
+
+        private float speed = 5f;
+
+        private Vector3 Pos
+        {
+            get => transform.position;
+            set => transform.position = value;
+        }
+
+        private Rigidbody2D rb;
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            SplineUtility.GetNearestPoint(path.Spline, Pos, out var nearest, out var t);
+            Pos = nearest;
+
+            var forward = path.EvaluateTangent(t);
+            var up = path.EvaluateUpVector(t);
+
+            transform.rotation = Quaternion.LookRotation(up, forward);
+            
+            rb.velocity = speed * transform.up;
+        }
+    }
+}
--- TowerDefense/Assets/Scripts/WaypointSystem/Waypoint.cs
+++ TowerDefense/Assets/Scripts/WaypointSystem/Waypoint.cs
@@ -4,14 +4,12 @@ namespace WaypointSystem
 {
     public class Waypoint : MonoBehaviour
     {
-        private Waypoint _next;
-
         [SerializeField]
-        private GameObject nextWaypoint;
+        private Waypoint nextWaypoint;
 
-        public Waypoint Next => _next ??= nextWaypoint ? nextWaypoint.GetComponent<Waypoint>() : null;
-
-        public bool IsLast => _next is null;
+        public Waypoint Next => nextWaypoint;
+        
+        public bool IsLast => nextWaypoint is null;
         public Vector3 Pos => transform.position;
     }
 }
