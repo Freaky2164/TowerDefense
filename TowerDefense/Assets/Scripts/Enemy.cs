cd56c600bb00b55b4973f29e08e4ee10ad773306
Freaky2164 <64361270+Freaky2164@users.noreply.github.com>
2023-04-16 15:53:37 +0200

    Added sound effects for buttons and killing enemies

---
+++ TowerDefense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private FollowWayPoint followWayPoint;
+    
+    public int ID { get; set; }
+
+    public int health;
+    public int value;
+    public int damage;
+
+    public AudioClip audioClip;
+
+    private void Start()
+    {
+        followWayPoint = GetComponent<FollowWayPoint>();
+        followWayPoint.LastWaypointReached += OnLastWaypointReached;
+    }
+
+    public void Damage(int damage)
+    {
+        health -= damage;
+    }
+
+    public bool HasHealthLeft()
+    {
+        return health > 0;
+    }
+
+    private void OnDestroy()
+    {
+        AudioSource.PlayClipAtPoint(audioClip, transform.position);
+        var gameHandler = GameObject.Find("GameHandler").GetComponent<GameHandler>();
+        gameHandler.EnemyDestroyed(ID, value);
+    }
+
+    private void OnLastWaypointReached()
+    {
+        GameHandler.i.Player.TakeDamage(damage);
+    }
+}
