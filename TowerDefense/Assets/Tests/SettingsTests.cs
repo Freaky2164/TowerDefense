1e6d6f138cd37c0f4613141e20a64e808e788a06
TimWaeckerle <97874901+TimWaeckerle@users.noreply.github.com>
2023-05-18 19:34:28 +0200

    Added tests for Settings

---
+++ TowerDefense/Assets/Tests/SettingsTests.cs
@@ -0,0 +1,35 @@
+﻿using GameHandling;
+using NUnit.Framework;
+using System.IO;
+using UnityEngine.Android;
+
+namespace Tests
+{
+    public class SettingsTests
+    {
+        private DirectoryInfo _settingsFolder;
+        private string _settingFile;
+
+        [SetUp]
+        public void Init()
+        {
+            _settingsFolder = JsonFileHandler.GetSettingFolder();
+            _settingFile = _settingsFolder.ToString() + Path.DirectorySeparatorChar + "Settings.json";
+            if(File.Exists(_settingFile))  File.Delete(_settingFile);
+            JsonFileHandler.Start();
+        }
+        
+        [Test]
+        public void SettingFileGenerationPasses()
+        {
+            Assert.IsTrue(File.Exists(_settingFile));
+        }
+
+        [Test]
+        public void SettingsFileContentPasses()
+        {
+            bool emptyFile = new FileInfo(_settingFile).Length == 0;
+            Assert.IsFalse(emptyFile);
+        }
+    }
+}
